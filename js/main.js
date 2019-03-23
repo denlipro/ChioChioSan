550c9f8cf4d9a3d0fa4079f58006a2c9d2366429
Denis Lipkovich <info@denli.pro>
2019-03-23 19:31:28 +0300

    ChioChioSan

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function openCall() {
+	document.getElementsByClassName('overlay')[0].style.display = "flex";
+	document.getElementsByClassName('overlay')[0].style.height = "100%";
+}
+
+function closeCall() {
+	document.getElementsByClassName('overlay')[0].style.display = "none";
+	document.getElementsByClassName('overlay')[0].style.height = "0%";
+}
